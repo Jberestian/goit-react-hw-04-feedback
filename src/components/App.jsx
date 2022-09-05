52ccc195e8b89a15611964121e57756b0fb4cd78
Jenia Berestian <jberestian@gmail.com>
2022-09-05 20:49:48 +0300

    added hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,57 +3,57 @@ import Feedback from './Feedback/Feedback';
 
 import '../reset.css';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    if (!total) {
-      return 0;
-    }
-    const { good } = this.state;
-    const result = Math.round((good / total) * 100);
+//   countPositiveFeedbackPercentage = () => {
+//     const total = this.countTotalFeedback();
+//     if (!total) {
+//       return 0;
+//     }
+//     const { good } = this.state;
+//     const result = Math.round((good / total) * 100);
 
-    return result;
-  };
+//     return result;
+//   };
 
-  leaveVote = propName => {
-    this.setState(prevState => {
-      const value = prevState[propName];
-      return {
-        [propName]: value + 1,
-      };
-    });
-  };
+//   leaveVote = propName => {
+//     this.setState(prevState => {
+//       const value = prevState[propName];
+//       return {
+//         [propName]: value + 1,
+//       };
+//     });
+//   };
 
-  render() {
-    const {
-      state,
-      countTotalFeedback,
-      countPositiveFeedbackPercentage,
-      leaveVote,
-    } = this;
+//   render() {
+//     const {
+//       state,
+//       countTotalFeedback,
+//       countPositiveFeedbackPercentage,
+//       leaveVote,
+//     } = this;
 
-    return (
-      <div>
-        <Feedback
-          state={state}
-          countTotalFeedback={countTotalFeedback}
-          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
-          leaveVote={leaveVote}
-        />
-      </div>
-    );
-  }
-}
+//     return (
+//       <div>
+//         <Feedback
+//           state={state}
+//           countTotalFeedback={countTotalFeedback}
+//           countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+//           leaveVote={leaveVote}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
